ca21a2a642067ad5f2d54fb4d9ca346ec1aaa3b9
tripham2323 <tripd.24itb@vku.udn.vn>
2025-10-13 01:29:34 +0700

    configure soft delete product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -82,7 +82,11 @@ module.exports.changeMulti = async (req, res) => {
 module.exports.deleteItem = async (req, res) => {
   const id = req.params.id;
   
-  await Product.deleteOne({ _id: id });
-
+  // await Product.deleteOne({ _id: id });
+  await Product.updateOne({ _id: id }, { 
+    deleted: true,
+    deleteAt: new Date()
+   });
+   
   res.redirect("/admin/products");
 };
