7de6ad70d892679fd46c853b3afeb86e89d46eb3
tripham2323 <tripd.24itb@vku.udn.vn>
2025-10-13 01:16:44 +0700

    add hard delete item

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -78,4 +78,11 @@ module.exports.changeMulti = async (req, res) => {
   res.redirect("/admin/products");
 };
 
+// [DELETE] /admin/products/delete/:id
+module.exports.deleteItem = async (req, res) => {
+  const id = req.params.id;
+  
+  await Product.deleteOne({ _id: id });
 
+  res.redirect("/admin/products");
+};
--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -22,3 +22,29 @@ if(buttonChangeStatus.length > 0) {
 }
 
 // End Status
+
+// Delete item 
+
+const buttonDelete = document.querySelectorAll("[button-delete]");
+if(buttonDelete.length > 0) {
+    const formDeleteItem = document.querySelector("#form-delete-item");
+    const path = formDeleteItem.getAttribute("data-path");
+
+    buttonDelete.forEach(button => {
+        button.addEventListener("click", () => {
+            const isConfirm = confirm("Bạn có chắc muốn xoá sản phẩm không?");
+
+            if(isConfirm) {
+                const id = button.getAttribute("data-id");
+
+                const action = `${path}/${id}?_method=DELETE`;
+
+                formDeleteItem.action = action;
+
+                formDeleteItem.submit();
+            }
+        })
+    })
+}
+
+// End Delete item
