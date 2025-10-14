1823788f9793888bc0d8d6e76780e0a1bbad2e51
tripham2323 <tripd.24itb@vku.udn.vn>
2025-10-14 20:39:33 +0700

    add detail product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -207,3 +207,23 @@ module.exports.editPatch = async (req, res) => {
 
   res.redirect(`${systemConfig.prefixAdmin}/products/edit/${req.params.id}`);
 };
+
+
+// [GET] /admin/products/detail/:id
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      _id: req.params.id,
+      deleted: false,
+    };
+
+    const product = await Product.findOne(find);
+
+    res.render("admin/pages/products/detail", {
+      pageTitle: product.title,
+      product: product
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/products`);
+  }
+};
