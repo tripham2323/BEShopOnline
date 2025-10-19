d7a4749289b0486a86de0ad3d3e9b19bcce45525
tripham2323 <tripd.24itb@vku.udn.vn>
2025-10-19 16:04:44 +0700

    add permission view and function

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -36,3 +36,65 @@ module.exports.createPost = async (req, res) => {
 
   res.redirect(`${systemConfig.prefixAdmin}/roles`);
 };
+
+// [POST] /admin/roles/edit
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    let find = {
+      _id: id,
+      deleted: false,
+    };
+
+    const data = await Role.findOne(find);
+
+    res.render("admin/pages/roles/edit", {
+      pageTitle: "Sửa nhóm quyền",
+      data: data,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+  }
+};
+
+// [PATCH] /admin/roles/edit
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Role.updateOne({ _id: id }, req.body);
+
+    req.flash("success", "Cập nhật nhóm quyền thành công");
+  } catch (error) {
+    req.flash("error", "Cập nhật nhóm quyền thất bại");
+  }
+  res.redirect(`${systemConfig.prefixAdmin}/roles/edit/${req.params.id}`);
+};
+
+// [GET] /admin/roles/permissions
+module.exports.permissions = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+
+  const records = await Role.find(find);
+
+  res.render("admin/pages/roles/permissions", {
+    pageTitle: "Phân quyền",
+    records: records,
+  });
+};
+
+// [PATCH] /admin/roles/permissions
+module.exports.permissionsPatch = async (req, res) => {
+  const permissions = JSON.parse(req.body.permissions);
+
+  for (const item of permissions) {
+    await Role.updateOne({ _id: item.id }, { permissions: item.permissions });
+  }
+
+  req.flash("success", "Cập nhật phân quyền thành công");
+  
+  res.redirect(`${systemConfig.prefixAdmin}/roles/permissions`);
+};
