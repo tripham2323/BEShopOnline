36354e2d3c5392fb3ec558ee07a4a80c554c6490
tripham2323 <tripd.24itb@vku.udn.vn>
2025-11-01 00:57:07 +0700

    add new product list in client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -9,11 +9,23 @@ module.exports.index = async (req, res) => {
     status: "active",
   }).limit(6);
 
-  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+  const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
 
   // Hết lấy ra sản phẩm nổi bật
+
+  // Lấy ra sản phẩm mới nhất
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(6);
+  const newProductsNew = productsHelper.priceNewProducts(productsNew);
+  // Hết lấy ra sản phẩm mới nhất
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    productsFeatured: newProducts,
+    productsFeatured: newProductsFeatured,
+    productsNew: newProductsNew,
   });
 };
