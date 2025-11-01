34dc4c7ecabb89eb39b76c11ba8500da797709b2
tripham2323 <tripd.24itb@vku.udn.vn>
2025-11-01 18:20:30 +0700

    search bar in client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,5 +1,6 @@
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
+const searchRoutes = require("./search.route");
 const categoryMiddleware = require("../../middlewares/clients/category.middleware");
 
 module.exports = (app) => {
@@ -8,4 +9,6 @@ module.exports = (app) => {
   app.use("/", homeRoutes);
 
   app.use("/products", productRoutes);
+
+  app.use("/search", searchRoutes);
 };
