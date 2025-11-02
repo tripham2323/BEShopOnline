7805f93a91d812cf44648c79c2720e42a589469f
tripham2323 <tripd.24itb@vku.udn.vn>
2025-11-02 20:46:28 +0700

    checkout success page

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -91,9 +91,28 @@ module.exports.order = async (req, res) => {
 
 // [GET] /checkout/success/:orderId
 module.exports.success = async (req, res) => {
-    console.log(req.params.orderId);
+  const order = await Order.findOne({
+    _id: req.params.orderId,
+  });
 
-    res.render("client/pages/checkout/success", {
-      pageTitle: "Đặt hàng thành công",
-    });
+  for (const product of order.products) {
+    const productInfo = await Product.findOne({
+      _id: product.product_id,
+    }).select("title thumbnail");
+    product.productInfo = productInfo;
+
+    product.priceNew = productHelper.priceNewProduct(product);
+
+    product.totalPrice = product.priceNew * product.quantity;
+  }
+
+  order.totalPrice = order.products.reduce(
+    (sum, item) => sum + item.totalPrice,
+    0
+  );
+
+  res.render("client/pages/checkout/success", {
+    pageTitle: "Đặt hàng thành công",
+    order: order,
+  });
 };
