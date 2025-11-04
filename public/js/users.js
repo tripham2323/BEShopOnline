2290c27916c28a5f60dd06eecf3a98996e7a6d9c
tripham2323 <tripd.24itb@vku.udn.vn>
2025-11-05 01:21:08 +0700

    accept friend

--- public/js/users.js
+++ public/js/users.js
@@ -40,3 +40,17 @@ if (listBtnRefuseFriend.length > 0) {
   });
 }
 // Hết Chức năng từ chối kết bạn
+
+// Chức năng chấp nhận kết bạn
+const listBtnAcceptFriend = document.querySelectorAll("[btn-accept-friend]");
+if (listBtnAcceptFriend.length > 0) {
+  listBtnAcceptFriend.forEach((button) => {
+    button.addEventListener("click", () => {
+      button.closest(".box-user").classList.add("accepted");
+      const userId = button.getAttribute("btn-accept-friend");
+
+      socket.emit("CLIENT_ACCEPT_FRIEND", userId);
+    });
+  });
+}
+// Hết Chức năng chấp nhận kết bạn
--- sockets/client/users.socket.js
+++ sockets/client/users.socket.js
@@ -86,7 +86,7 @@ module.exports = (res) => {
       }
     });
 
-    // Chức năng từ chối kết bạn 
+    // Chức năng từ chối kết bạn
     socket.on("CLIENT_REFUSE_FRIEND", async (userId) => {
       const myUserId = res.locals.user.id;
 
@@ -120,6 +120,63 @@ module.exports = (res) => {
             _id: userId,
           },
           {
+            $pull: {
+              requestFriends: myUserId,
+            },
+          }
+        );
+      }
+    });
+
+    // Chức năng chấp nhận kết bạn
+    socket.on("CLIENT_ACCEPT_FRIEND", async (userId) => {
+      const myUserId = res.locals.user.id;
+
+      // Thêm {user_id, room_chat_id} của A vào friendList của B
+      // Xoá id của A trong accepFriends của B
+      const existIdAinB = await User.findOne({
+        _id: myUserId,
+        acceptFriends: userId,
+      });
+
+      if (existIdAinB) {
+        await User.updateOne(
+          {
+            _id: myUserId,
+          },
+          {
+            $push: {
+              friendList: {
+                user_id: userId,
+                room_chat_id: "",
+              },
+            },
+            $pull: {
+              acceptFriends: userId,
+            },
+          }
+        );
+      }
+
+      // Thêm {user_id, room_chat_id} của B vào friendList của A
+      // Xoá id của B trong requestFriends của A
+      const existIdBinA = await User.findOne({
+        _id: userId,
+        requestFriends: myUserId,
+      });
+
+      if (existIdBinA) {
+        await User.updateOne(
+          {
+            _id: userId,
+          },
+          {
+            $push: {
+              friendList: {
+                user_id: myUserId,
+                room_chat_id: "",
+              },
+            },
             $pull: {
               requestFriends: myUserId,
             },
